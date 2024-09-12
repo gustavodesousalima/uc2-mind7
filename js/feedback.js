ec0a912a0fc8f4bc53d42912ca21a3db0cd4b1ac
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2024-09-12 03:48:27 -0300

    Update

--- js/feedback.js
+++ js/feedback.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.swiper-container', {
-        loop: true, // Habilitar loop infinito
-        slidesPerView: 3, // Número de slides a serem exibidos por vez
-        spaceBetween: 0, // Espaço entre os slides
-        centeredSlides: false, // Não centraliza slides
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 0,
+        centeredSlides: false,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
--- js/logarconta.js
+++ js/logarconta.js
@@ -1,9 +1,7 @@
 function logarconta() {
-    // Get values from input fields
     var senha = document.querySelector("#senha").value;
     var email = document.querySelector("#email").value;
 
-    // Check credentials
     if (senha === "1234567890" && email === "gustavo@gmail.com") {
         window.location.href = "./usuario.html";
     } else {
